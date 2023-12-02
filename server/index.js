6573acd1af51058490a9fef165d33ba0ce7f5810
quangchinh186 <quangchinh1122@gmail.com>
2023-12-02 23:38:19 +0700

    init

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const UserModel = require('./schema/user')
 
 const app = express();
 app.use(cors());
@@ -10,4 +11,33 @@ mongoose.connect("mongodb+srv://quangchinh1122:chinh2003@uetinder.bmmhsoe.mongod
 
 app.listen(3001, () => {
     console.log('server running on 3001');
+})
+
+app.post('/createUser', (req, res) => {
+    UserModel.find({"username": req.body.username})
+    .then(users => {
+      if (users.length !== 0) {
+        res.json('Username is taken!!!')
+      } else {
+        const newUser = new usersModel({
+            username: req.body.username,
+            password: req.body.password,
+        });
+        newUser.save();
+        res.json(newUser._id);
+      }
+    })
+    .catch(err => res.json(err))
+})
+
+app.get('/', (req, res) => {
+    UserModel.find({}).then(result => res.json(result)).catch(err => res.json(err))
+})
+
+app.put('/editUser', (req, res) => {
+
+})
+
+app.delete('/deleteUser', (req, res) => {
+    
 })
